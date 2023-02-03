919c03a4be725c1f43f7653ff758553edefc072d
Prateek Dhama <prateekdhama@L-W4V5R4JD77.local>
2023-02-03 15:11:14 +0530

    backend intergaration

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  users: any;
+  deleteUser: boolean = true;
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit(){
+   this.callFunc();
+  }
+
+  callFunc(){
+    this.http.get('http://localhost:3000/users/getUser').subscribe((res)=>{
+      this.users = res;
+      console.log(this.users)
+    })
+  }
+
+  delete(email: string){
+    let body = {email:email};
+    this.http.post('http://localhost:3000/users/deleteUser',body).subscribe((res)=>{
+      // this.users = res;
+      console.log(res);
+      this.callFunc()
+    })
+  }
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
