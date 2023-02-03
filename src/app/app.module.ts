73920b8de0c2df3b944890153734bfd5775e74b5
Karan Bengani <karanbengani@L-LTLF2NVJ4T.local>
2023-02-03 15:10:34 +0530

    nav bar changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { BlogmodalComponent } from './blogmodal/blogmodal.component';
 import { NgxTypedJsModule } from 'ngx-typed-js';
+// import { NgxTypedJsModule } from 'ngx-typed-js';
 import { AuthFormComponent } from './auth-form/auth-form.component';
 import { AdminComponent } from './admin/admin.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,6 +23,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -48,7 +50,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatCardModule,
     MatProgressBarModule,
     MatDividerModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
