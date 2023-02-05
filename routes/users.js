8a59a21bfa3fda75ecea396932d04198988123d6
Hardik Garg <hardikgarg@L-ND725XHK5Q.local>
2023-02-05 13:10:57 +0530

    finished

--- routes/users.js
+++ routes/users.js
@@ -33,64 +33,13 @@ const transporter = nodemailer.createTransport({
   // },
 });
 
-function send_mail(to, subject, body, callback) {
+function send_mail(to, subject, body, attachments, callback) {
   // send email
   let send_to = {
     from: "admiresecret71@gmail.com",
     to: to,
     subject: subject,
-    attachments: [
-      {
-        filename: "64951510234941531.png",
-        path: __dirname + "/../images/64951510234941531.png",
-        cid: "64951510234941531",
-      },
-      {
-        filename: "facebook-logo-black.png",
-        path: __dirname + "/../images/facebook-logo-black.png",
-        cid: "facebook-logo-black",
-      },
-      {
-        filename: "following_2.gif",
-        path: __dirname + "/../images/following_2.gif",
-        cid: "following_2",
-      },
-      {
-        filename: "group_10_DPF.png",
-        path: __dirname + "/../images/group_10_DPF.png",
-        cid: "group_10_DPF",
-      },
-      {
-        filename: "instagram-logo-black.png",
-        path: __dirname + "/../images/instagram-logo-black.png",
-        cid: "instagram-logo-black",
-      },
-      {
-        filename: "pinterest-logo-black.png",
-        path: __dirname + "/../images/pinterest-logo-black.png",
-        cid: "pinterest-logo-black",
-      },
-      {
-        filename: "telegram-logo-black.png",
-        path: __dirname + "/../images/telegram-logo-black.png",
-        cid: "telegram-logo-black",
-      },
-      {
-        filename: "tiktok-logo-black.png",
-        path: __dirname + "/../images/tiktok-logo-black.png",
-        cid: "tiktok-logo-black",
-      },
-      {
-        filename: "twitter-logo-black.png",
-        path: __dirname + "/../images/twitter-logo-black.png",
-        cid: "twitter-logo-black",
-      },
-      {
-        filename: "youtube-logo-black.png",
-        path: __dirname + "/../images/youtube-logo-black.png",
-        cid: "youtube-logo-black",
-      },
-    ],
+    attachments: attachments,
     html: body,
   };
   // console.log(send_to);
@@ -110,7 +59,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/getUser',(req,res)=>{
-  client.query("select * from users", (err, data) => {
+  client.query("select * from users where name!='Admin'", (err, data) => {
       if(!err) {
           res.send(data.rows);
       }
@@ -142,7 +91,59 @@ router.post('/newUser',(req,res)=>{
                 let subject="You have subscribed to News-App";
                 let to=req.body.email;
                 bod = bod.replace("{{user_name}}", req.body.name);
-                send_mail(to, subject, bod, (err, data) => {
+                let attachments = [
+                  {
+                    filename: "64951510234941531.png",
+                    path: __dirname + "/../images/64951510234941531.png",
+                    cid: "64951510234941531",
+                  },
+                  {
+                    filename: "facebook-logo-black.png",
+                    path: __dirname + "/../images/facebook-logo-black.png",
+                    cid: "facebook-logo-black",
+                  },
+                  {
+                    filename: "following_2.gif",
+                    path: __dirname + "/../images/following_2.gif",
+                    cid: "following_2",
+                  },
+                  {
+                    filename: "group_10_DPF.png",
+                    path: __dirname + "/../images/group_10_DPF.png",
+                    cid: "group_10_DPF",
+                  },
+                  {
+                    filename: "instagram-logo-black.png",
+                    path: __dirname + "/../images/instagram-logo-black.png",
+                    cid: "instagram-logo-black",
+                  },
+                  {
+                    filename: "pinterest-logo-black.png",
+                    path: __dirname + "/../images/pinterest-logo-black.png",
+                    cid: "pinterest-logo-black",
+                  },
+                  {
+                    filename: "telegram-logo-black.png",
+                    path: __dirname + "/../images/telegram-logo-black.png",
+                    cid: "telegram-logo-black",
+                  },
+                  {
+                    filename: "tiktok-logo-black.png",
+                    path: __dirname + "/../images/tiktok-logo-black.png",
+                    cid: "tiktok-logo-black",
+                  },
+                  {
+                    filename: "twitter-logo-black.png",
+                    path: __dirname + "/../images/twitter-logo-black.png",
+                    cid: "twitter-logo-black",
+                  },
+                  {
+                    filename: "youtube-logo-black.png",
+                    path: __dirname + "/../images/youtube-logo-black.png",
+                    cid: "youtube-logo-black",
+                  },
+                ]
+                send_mail(to, subject, bod, attachments, (err, data) => {
                   console.log(data);
                 });
                 res.status(200).send({
@@ -246,7 +247,8 @@ router.post('/adminMail',(req,res)=>{
   let subject=req.body.subject;
   let bod=req.body.bod;
   let to="hardikgarg01@gmail.com";
-  send_mail(to, subject, bod, (err, data) => {
+  let attachments = []
+  send_mail(to, subject, bod, attachments, (err, data) => {
     console.log(data);
   });
 })
