065dd352c24f9cafbff7fc85e65a493e58c4bfd5
Prateek Dhama <prateekdhama@L-W4V5R4JD77.local>
2023-02-06 09:38:02 +0530

    admin bug

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,75 +1,76 @@
-import { Component , Output,EventEmitter} from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { StoreService } from '../store.service';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css', '../auth-form/auth-form.component.css']
+  styleUrls: [
+    './nav-bar.component.css',
+    '../auth-form/auth-form.component.css',
+  ],
 })
 export class NavBarComponent {
   @Output() newsEvent = new EventEmitter<boolean>();
   @Output() navLinkEvent = new EventEmitter<string>();
 
-  activeClass : boolean = false;
+  activeClass: boolean = false;
   isLoggedIn = false;
   admin = false;
 
-  constructor(public store: StoreService,private router: Router){}
+  constructor(public store: StoreService, private router: Router) {}
 
-  navClick(){
-    this.newsEvent.emit(true)
+  navClick() {
+    this.newsEvent.emit(true);
   }
-  navNews(cat:string){
+  navNews(cat: string) {
     this.store.category = cat;
-    console.log(this.store.category)
-    this.navLinkEvent.emit('news')
-    this.router.navigate(['main/blog'])
+    console.log(this.store.category);
+    this.navLinkEvent.emit('news');
+    this.router.navigate(['main/blog']);
   }
-  navAbout(){
-    this.navLinkEvent.emit('about')
-    this.router.navigate(['main/about'])
+  navAbout() {
+    this.navLinkEvent.emit('about');
+    this.router.navigate(['main/about']);
   }
-  navPhoto(){
-    this.navLinkEvent.emit('photo')
-    this.router.navigate(['main/photo'])
+  navPhoto() {
+    this.navLinkEvent.emit('photo');
+    this.router.navigate(['main/photo']);
   }
-  navLog(){
-    this.navLinkEvent.emit('log')
-    this.router.navigate(['main/auth'])
+  navLog() {
+    this.navLinkEvent.emit('log');
+    this.router.navigate(['main/auth']);
   }
-  navAdmin(){
-    this.navLinkEvent.emit('admin')
-    this.router.navigate(['main/admin'])
+  navAdmin() {
+    this.navLinkEvent.emit('admin');
+    this.router.navigate(['main/admin']);
   }
-  ngOnInit(blog:any){
-  this.activeClass = false
-    console.log(localStorage.getItem("eweekly_user"));
-    if(localStorage.getItem("eweekly_user")!=null){
-      
-      this.isLoggedIn = true
+  ngOnInit(blog: any) {
+    this.activeClass = false;
+    console.log(localStorage.getItem('eweekly_user'));
+    if (localStorage.getItem('eweekly_user') != null) {
+      this.isLoggedIn = true;
       this.admin = false;
     }
-    if(localStorage.getItem("eweekly_user")=="admin@admin.com"){
-      
-      this.isLoggedIn = true
+    if (localStorage.getItem('eweekly_user') == 'admin@admin.com') {
+      this.isLoggedIn = true;
       this.admin = true;
     }
-    if(localStorage.getItem("eweekly_user")==null){
+    if (localStorage.getItem('eweekly_user') == null) {
       this.isLoggedIn = false;
     }
-   }
-   
-  toggleForm(){
-    this.activeClass= !this.activeClass
   }
 
-  logout(){
-    localStorage.removeItem("eweekly_user");
+  toggleForm() {
+    this.activeClass = !this.activeClass;
+  }
+
+  logout() {
+    localStorage.removeItem('eweekly_user');
     this.isLoggedIn = false;
-    this.admin=false;
-    this.navLinkEvent.emit('news')
-    this.router.navigate(['blog'])
-    alert("You just logged out");
+    this.admin = false;
+    this.navLinkEvent.emit('news');
+    this.router.navigate(['main/blog']);
+    alert('You just logged out');
   }
 }
